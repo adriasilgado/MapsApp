f6aae642ea71ad95dfb7bb4c1c1c0398627d2aba
adriasilgado <adria.silgado.7e7@itb.cat>
2024-03-26 15:48:16 +0100

    Guardar foto desde galeria

--- app/src/main/java/com/example/mapsapp/view/GalleryScreen.kt
+++ app/src/main/java/com/example/mapsapp/view/GalleryScreen.kt
@@ -12,10 +12,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,6 +38,8 @@ import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.NavController
 import com.example.mapsapp.R
+import com.example.mapsapp.navigation.Routes
+import com.example.mapsapp.sky
 import com.example.mapsapp.viewModel.MyViewModel
 
 @Composable
@@ -51,7 +57,7 @@ fun GalleryScreen(navigationController: NavController, myViewModel: MyViewModel)
                     ImageDecoder.createSource(context.contentResolver, it1)
                 }
                 source?.let { it1 -> ImageDecoder.decodeBitmap(it1)
-                }!!
+                }
             }
         })
     Column(
@@ -61,8 +67,13 @@ fun GalleryScreen(navigationController: NavController, myViewModel: MyViewModel)
     ) {
         Button(onClick = {
             launchImage.launch("image/*")
-        }) {
-            Text("Open Gallery")
+        },
+            modifier = Modifier
+                .fillMaxHeight(0.05f)
+                .width(150.dp),
+            shape = RoundedCornerShape(25.dp),
+            colors = ButtonDefaults.buttonColors(Color.DarkGray)) {
+            Text("Open Gallery", fontFamily = sky)
         }
         Image(
             bitmap = if (bitmap != null) bitmap!!.asImageBitmap() else img!!.asImageBitmap(), contentDescription = null,
@@ -71,5 +82,17 @@ fun GalleryScreen(navigationController: NavController, myViewModel: MyViewModel)
                 .background(Color.Transparent)
                 .border(width = 1.dp, color = Color.White, shape = CircleShape)
         )
+        Button(onClick = {
+            myViewModel.changePhotoMarker(bitmap!!)
+            navigationController.navigate(Routes.MapScreen.route)
+        },
+            modifier = Modifier
+                .fillMaxHeight(0.1f)
+                .width(150.dp),
+            shape = RoundedCornerShape(25.dp),
+            colors = ButtonDefaults.buttonColors(Color.DarkGray),
+            enabled = bitmap != null) {
+            Text("Save", fontFamily = sky)
         }
+    }
 }
