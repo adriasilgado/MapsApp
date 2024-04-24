da6001a4e51b2a40e5024045c68becdbcc52faf1
adria.silgado.7e7 <adria.silgado.7e7@itb.cat>
2024-04-24 10:04:55 +0200

    detalle con mapa

--- app/src/main/java/com/example/mapsapp/view/DetailScreen.kt
+++ app/src/main/java/com/example/mapsapp/view/DetailScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -33,10 +34,20 @@ import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.mapsapp.R
+import com.example.mapsapp.model.Marca
 import com.example.mapsapp.navigation.Routes
 import com.example.mapsapp.sky
 import com.example.mapsapp.viewModel.MyViewModel
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -55,7 +66,9 @@ fun DetailScreen(markerId:String, navigationController: NavController, myViewMod
                 )
             }
             Column(
-                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
@@ -78,20 +91,15 @@ fun DetailScreen(markerId:String, navigationController: NavController, myViewMod
                 }
                 Spacer(modifier = Modifier.height(8.dp))
                 Card(shape = RoundedCornerShape(4.dp), elevation = cardElevation(4.dp)) {
-                    Column {
-                        Text(
-                            text = "Latitude: ${myViewModel.posMarker.value?.latitude}",
-                            fontFamily = sky,
-                            color = Color.Black,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                        Text(
-                            text = "Longitude: ${myViewModel.posMarker.value?.longitude}",
-                            fontFamily = sky,
-                            color = Color.Black,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    }
+                    DetailMap(Marca(
+                        myViewModel.userId.value,
+                        myViewModel.markerId.value,
+                        myViewModel.posMarker.value!!.latitude,
+                        myViewModel.posMarker.value!!.longitude,
+                        myViewModel.nameMaker.value!!,
+                        myViewModel.typeMarker.value!!,
+                        myViewModel.photoMarker.value
+                    ), myViewModel)
                 }
                 Spacer(modifier = Modifier.height(16.dp))
 
@@ -119,7 +127,8 @@ fun DetailScreen(markerId:String, navigationController: NavController, myViewMod
                                 .clickable {
                                     myViewModel.changeMarkerId(markerId)
                                     myViewModel.setIsAddImage(true)
-                                    navigationController.navigate(Routes.CameraScreen.route) }
+                                    navigationController.navigate(Routes.CameraScreen.route)
+                                }
                                 .background(Color.DarkGray)
                                 .padding(8.dp)
                         )
@@ -129,3 +138,36 @@ fun DetailScreen(markerId:String, navigationController: NavController, myViewMod
         }
     }
 }
+
+@Composable
+fun DetailMap(marker: Marca, myViewModel: MyViewModel) {
+    val position = LatLng(marker.lat, marker.lon)
+    GoogleMap(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .clip(RoundedCornerShape(25.dp)),
+        cameraPositionState = CameraPositionState(CameraPosition(position, 16f, 0f, 0f)),
+        uiSettings = MapUiSettings(
+            compassEnabled = false,
+            tiltGesturesEnabled = false,
+            myLocationButtonEnabled = false,
+            zoomControlsEnabled = false,
+            zoomGesturesEnabled = false,
+            scrollGesturesEnabled = false
+        ),
+        properties = MapProperties(
+            isBuildingEnabled = true,
+            mapType = MapType.NORMAL,
+            isMyLocationEnabled = false,
+        )
+    ) {
+        val pos:LatLng = LatLng(marker.lat, marker.lon)
+        Marker(
+            state = MarkerState(position = pos),
+            title = marker.name,
+            icon = BitmapDescriptorFactory.fromResource(myViewModel.whatIcon(marker.tipo))
+        )
+    }
+}
+
